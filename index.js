55e6fd3a7695aa3a068fbbc953a426b8d2621e62
Brennan Colberg <brennan.colberg@gmail.com>
2018-08-23 22:55:32 -0700

    Blog content implementation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 	function ce(tag) { return document.createElement(tag); }
 	
 	window.addEventListener("load", function() {
+		
 		// ajax code gotten from CSE 154, hosted on personal repository
 		ajaxGET("projects.json", function(json) {
 			let div = $("projects");
@@ -53,9 +54,60 @@
 				
 			}
 		});
-		ajaxGET("blog.json", function() {
-			
+		
+		ajaxGET("blog.json", function(json) {
+			console.log("going");
+			let data = JSON.parse(json);
+			for (let i = 0; i < data.length; i++) {
+				
+				let datum = data[i];
+				let post = ce("article");
+				
+				// title
+				let titleSection = ce("section");
+				titleSection.className = "title";
+				if (datum["title"]) {
+					let title = ce("h2");
+					title.textContent = datum["title"];
+					titleSection.appendChild(title);
+				}
+				if (datum["subtitle"]) {
+					let subtitle = ce("h4");
+					subtitle.textContent = datum["subtitle"];
+					titleSection.appendChild(subtitle);
+				}
+				if (titleSection.childElementCount > 0) {
+					post.appendChild(titleSection);
+				}
+				
+				// links
+				if (datum["paragraphs"]) {
+					let paragraphSection = ce("section");
+					paragraphSection.className = "paragraphs";
+					for (let l = 0; l < datum["paragraphs"].length; l++) {
+						let p = ce("p");
+						p.textContent = datum["paragraphs"][l];
+						paragraphSection.appendChild(p);
+					}
+					post.appendChild(paragraphSection);
+				}
+				
+				// description
+				if (datum["link"]) {
+					let linkSection = ce("section");
+					linkSection.className = "link";
+					let link = ce("a");
+					link.href = datum["link"];
+					link.textContent = "[... read more ...]";
+					linkSection.appendChild(link);
+					post.appendChild(linkSection);
+				}
+				
+				$("blog").appendChild(post);
+				
+			}
 		});
+		
 	});
 	
 })();

8d9849819467886bb55bbdb5aa5043478165f641
Brennan Colberg <brennan.colberg@gmail.com>
2018-09-24 23:50:08 -0700

    replaced root index file with PHP

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<head>
+		<title>Brennan Colberg</title>
+		<link href="../style.css" rel="stylesheet" />
+		<!--<script src="../content.js"></script>-->
+	</head>
+
+	<body id="page-home">
+		
+		<!-- body header (with my name!) -->
+		<header>
+			<h1>Brennan Colberg</h1>
+			<nav id="menu">
+				<!-- generated with index.js from menu.json -->
+			</nav>
+		</header>
+		
+		<p id="introduction">
+			<strong>Hello, my name is Brennan!</strong> Welcome to my website; this is the second website I've made (the first one was for <a href="https://frc3268.github.io" target="_blank">my robotics team</a>). I'm a first-year Computer Science student at the University of Washington in Seattle. I program a lot in my spare time -- look at the projects down below! I want to start posting blogs as well, soon -- look at the placeholder blog area down below too!
+		</p>
+		
+		<h2>Personal Projects</h2>
+		<div id="projects" class="feed">
+			<!-- generated with index.js from projects/projects.json -->
+		</div>
+		
+		<footer>
+			<div id="contact">
+				<!-- generated with index.js from contact.json -->
+			</div>
+			<p>&copy; Brennan Colberg 2018</p>
+		</footer>
+		
+	</body>
+</html>

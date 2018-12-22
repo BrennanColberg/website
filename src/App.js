3c5bdcf0dbcc54796f198bd0be7a7f79740364cd
Brennan Colberg <brennan.colberg@gmail.com>
2018-12-21 22:34:36 -0800

    deployment script (w/surge.sh)

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Testing surge.sh deployment! 2
           </a>
         </header>
       </div>
---
+++ surge.sh
@@ -0,0 +1,4 @@
+npm run build
+cd build
+surge
+cd ..

73a04ba2e0ceed3217cf2fda49d41bde32e9311a
Brennan Colberg <brennan.colberg@gmail.com>
2018-12-22 00:33:57 -0800

    menu auto-generation

--- src/Menu.js
+++ src/Menu.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
+import menuJSON from './assets/menu.json';
 
-class Menu extends Component {
-  render() {
-    return (
-      <nav className="Menu">
-        menu
-      </nav>
-    );
-  }
-}
+const Menu = () => (
+  <nav className="Menu">
+    {menuJSON.map(item =>
+      <a href={item.href} key={item.text}>{item.text}</a>
+    )}
+  </nav>
+);
 
 export default Menu;

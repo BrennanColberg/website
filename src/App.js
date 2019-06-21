883434ce537411d41260c749f9582ce10be52f9c
Brennan Colberg <brennan.colberg@gmail.com>
2019-06-21 10:13:01 -0700

    minor changes for heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import "./App.scss";
 
 import React from "react";
-import { HashRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Books from "./Books";
 
 export default () => (
-  <Router>
-    <>
-      <Route exact path="/" component={Home} />
-      <Route path="/books" component={Books} />
-    </>
-  </Router>
+	<Router>
+		<>
+			<header>
+				<h3>
+					<Link to="/">BDC</Link>
+				</h3>
+				<Link to="/books">Books</Link>
+			</header>
+			<main>
+				<Route exact path="/" component={Home} />
+				<Route path="/books" component={Books} />
+			</main>
+			<footer>&copy; Brennan Colberg 2019. All rights reserved.</footer>
+		</>
+	</Router>
 );
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,2 +1,4 @@
 import React from "react";
-export default () => <>home</>;
+export default () => (
+	<h3>My name is Brennan Colberg; I'm an autodidact, among other things.</h3>
+);

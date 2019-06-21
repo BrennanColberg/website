b781209202d8a7ca073c2e4fdfebcd9cfa2d0435
Brennan Colberg <brennan.colberg@gmail.com>
2019-06-21 11:31:34 -0700

    book article edits

--- src/Books/index.js
+++ src/Books/index.js
@@ -4,6 +4,7 @@ import React from "react";
 
 import Book from "./Book";
 import json from "./books";
+import Paren from "../Paren";
 
 export default () => (
 	<>
@@ -20,10 +21,10 @@ export default () => (
 				I read a lot. Since June 2018, I've finished {json.length} books–from
 				novels to academic summaries, history to educational theory, classics to
 				self-help books, I tend to pick up anything that piques my interest. All
-				of those books are above, in chronological order of when I finished each
-				(if I read something twice, it's posted twice).
+				of those books are above, in chronological order of when I finished each{" "}
+				<Paren>if I read something twice, it's posted twice</Paren>.
 			</p>
-			<p>
+			<p class="hidden">
 				I've given each book a personal rating on a scale between -1.0 and 1.0
 				based on how likely I would be to recommend it. This rating is expressed
 				in each of the books' shadows; the greener a shadow is, the more I'd
@@ -31,12 +32,13 @@ export default () => (
 				no shadow, I haven't coded an opinion in for that book yet.
 			</p>
 			<p>
-				If you're interested in chatting about any of these books (whether from
-				personal curiosity, wanting to hear my take on them, or wanting to
-				chat), then{" "}
-				<a href="mailto:brennan.colberg@gmail.com">shoot me an email!</a> I'll
-				respond as fast as I can–and I'd love to grab coffee to chat about
-				almost anything in more depth if you're interested!
+				If you're interested in chatting about any of these books{" "}
+				<Paren>
+					whether from personal curiosity, wanting to hear my take on them, or
+					wanting to chat in more depth
+				</Paren>
+				, then <a href="mailto:brennan.colberg@gmail.com">shoot me an email!</a>{" "}
+				I'll respond as fast as I can.
 			</p>
 		</article>
 	</>

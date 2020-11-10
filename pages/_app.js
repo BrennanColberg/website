ffa6b9de59e022ceb1d02eb937a67ddbc4eedded
Brennan Colberg <brennan.colberg@gmail.com>
2020-11-10 08:50:52 -0800

    update next.js; add default <title>

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { firestoreClient } from '../data/firebase'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -11,12 +12,17 @@ function MyApp({ Component, pageProps }) {
   }, [])
 
   return (
-    <div
-      id="app"
-      className={router.pathname.slice(1).replace(/\//g, '-') || 'root'}
-    >
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Brennan Colberg</title>
+      </Head>
+      <div
+        id="app"
+        className={router.pathname.slice(1).replace(/\//g, '-') || 'root'}
+      >
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
 

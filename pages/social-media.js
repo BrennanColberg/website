fe30a341a0faf2176021674fc4ecad00b6abdc3d
Brennan Colberg <brennan.colberg@gmail.com>
2020-11-11 07:52:33 -0800

    moved social media profiles to be stored in Firebase

--- pages/social-media.js
+++ pages/social-media.js
@@ -1,14 +1,52 @@
 import Header from '../components/Header'
-import SocialMediaProfiles from '../components/SocialMediaProfiles'
+import { firestoreClient } from '../data/firebase'
+import styles from '../styles/SocialMediaProfiles.module.scss'
 
-const ContactPage = () => (
+const SocialMediaProfile = ({ profile }) => (
+  <a
+    style={{
+      '--color': profile.color,
+    }}
+    href={profile.link}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img src={`logos/${profile.name}.png`} alt={profile.name} />
+    <span>{profile.user}</span>
+  </a>
+)
+
+const ContactPage = ({ socialMediaProfiles }) => (
   <>
     <Header />
     <main>
       <h1>I'm "BrennanColberg" everywhere</h1>
-      <SocialMediaProfiles />
+
+      <div className={styles.socialMediaProfiles}>
+        {socialMediaProfiles
+          .filter(({ active }) => active)
+          .map((profile, i) => (
+            <SocialMediaProfile key={i} profile={profile} />
+          ))}
+      </div>
+      {/* <hr />
+      <div className={styles.socialMediaProfiles}>
+        {socialMediaProfiles
+          .filter(({ active }) => !active)
+          .map((profile, i) => (
+            <SocialMediaProfile key={i} profile={profile} />
+          ))}
+      </div> */}
     </main>
   </>
 )
+
+export async function getStaticProps() {
+  const snap = await firestoreClient.collection('social-media').get()
+  const socialMediaProfiles = snap.docs.map((doc) => doc.data())
+  return {
+    props: { socialMediaProfiles },
+  }
+}
 
 export default ContactPage

e5801205da250b56331027528245216eadc3cb66
Brennan Colberg <brennan.colberg@gmail.com>
2020-11-27 13:04:57 -0800

    rewrite homepage again

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,13 @@ const SplashPage = () => (
     <Markdown
       text={`
 
-# Welcome to my website!
+# Welcome; please poke around!
 
-I'm an autodidact (learn from [books](/books) as a primary hobby), programmer (like to make things), and miser (don't like to spend money).
+This is my repository of past projects, writing, and book notes (plus soon quotes); I try to publish something every day, but am pretty bad at it.
 
-I'm also procrastinating on putting more proper content on this website! 😊
+Right now, I'm in charge of tech at [WEEKDAYS](https://joinweekdays.com), a marketplace for micro-schools that focuses on quality above all else. (Each has 3-8 kids, with 1 teacher, in 1 home; it's similar to homeschooling, but not quite the same).
 
-Right now, I'm building [WEEKDAYS](https://joinweekdays.com), a marketplace for elementary-age micro-schools (each has 3-8 kids, with 1 teacher, in 1 home).
-
-I live in Corvallis, OR (but am extremely connected to Seattle).
+If you want to chat for any reason (writing, contracting, or socially), please don't hesitate to reach out via [email](mailto:brennan.colberg@gmail.com) or [Twitter](https://twitter.com/BrennanColberg)! I usually don't bite 🙃
 
 `}
     />
